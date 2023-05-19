9e028389e1fa9a9c3df5fb12999c9223dd43f843
mahamostaf <mahamoftah19@gmail.com>
2023-05-19 16:27:33 -0700

    task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabetical numbers
+ *
+ * Return - (0) on success
+ */
+
+int main()
+{
+for (char i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return 0;
+}
