503774c7ec5d760ecf4a610ffbcd1bb7c7deb6bd
mahamostaf <mahamoftah19@gmail.com>
2023-05-19 16:43:30 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabetical numbers except q and e
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
