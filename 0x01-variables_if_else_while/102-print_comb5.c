24df6d468ae44ba9e8d4a7024eb086220c054348
mahamostaf <mahamoftah19@gmail.com>
2023-05-19 17:51:41 -0700

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print 4 combinations
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+{
+if (i == j)
+continue;
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i == 98 && j == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
