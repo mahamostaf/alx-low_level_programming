df617d3fbabaeb9efd2cfad4f32ef91a784b4800
mahamostaf <mahamoftah19@gmail.com>
2023-05-23 16:59:11 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_alphabet_x10 - print alphapetical characters 10 times
+ *
+ * Return: void
+ */
+int _islower(int c)
+{
+    if (islower(c))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
