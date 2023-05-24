427493779e4f1cc584d684312b6d0323480daad2
mahamostaf <mahamoftah19@gmail.com>
2023-05-23 19:33:47 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 
 /**
@@ -13,35 +14,21 @@ if (n > 98)
 {
 for (i = n; i >= 98; i--)
 {
-if (i > 9)
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-}
-else
-_putchar(i + '0');
+printf("%d", i);
 if (i == 98)
 continue;
-_putchar(',');
-_putchar(' ');
+printf(", ");
 }
 }
 else
 for (i = n; i <= 98; i++)
 {
-if (i > 9)
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-}
-else
-_putchar(i + '0');
+printf("%d", i);
 if (i == 98)
 continue;
-_putchar(',');
-_putchar(' ');
+printf(", ");
 }
 
 _putchar('\n');
-}
 
+}
