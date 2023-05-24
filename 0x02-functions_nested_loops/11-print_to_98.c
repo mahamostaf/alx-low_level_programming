89ae017acc00e3096927249aee1ba0a36a41dd80
mahamostaf <mahamoftah19@gmail.com>
2023-05-23 19:18:57 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+/**
+ * print_to_98 - print from number n to 98
+ * @n: parameter
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+int i;
+if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+if (i > 9)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+else
+_putchar(i + '0');
+if (i == 98)
+continue;
+_putchar(',');
+_putchar(' ');
+}
+}
+else
+for (i = n; i <= 98; i++)
+{
+if (i > 9)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+else
+_putchar(i + '0');
+if (i == 98)
+continue;
+_putchar(',');
+_putchar(' ');
+}
+
+_putchar('\n');
+}
+
