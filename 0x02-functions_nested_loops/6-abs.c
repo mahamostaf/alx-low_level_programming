f6579cd896f0e0a3ea0accbbe584b90462776248
mahamostaf <mahamoftah19@gmail.com>
2023-05-23 17:42:09 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _abs - check sign of number
+ * @n: parameter to print_sign function
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+return (abs(n));
+}
