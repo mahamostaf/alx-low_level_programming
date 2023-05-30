a8ccd8351b058773b8f78f69fc11bbfaf5309f94
mahamostaf <mahamoftah19@gmail.com>
2023-05-30 15:12:29 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - print string to standard output
+ * @str: pointer s
+ * Return: void
+ */
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
