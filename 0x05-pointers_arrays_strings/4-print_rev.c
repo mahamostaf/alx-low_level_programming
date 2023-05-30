83a1857a0a5f70b26e1decf7dbc5ae8d81c34789
mahamostaf <mahamoftah19@gmail.com>
2023-05-30 15:24:41 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* print_rev - reverse string
+* @s: pointer s
+* Return: void
+*/
+void print_rev(char *s)
+{
+int i;
+for (i = strlen(s); i > 0; i--)
+{
+printf("%c", s[i]);
+}
+printf("\n");
+}
