8225d395ce54bd7e83959da3853f45750df359bf
mahamostaf <mahamoftah19@gmail.com>
2023-05-30 17:36:38 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* puts_half - print every other character
+* @str: pointer s
+* Return: void
+*/
+void puts_half(char *str)
+{
+int i;
+char *tmp = str;
+tmp += strlen(str) / 2;
+for (i = strlen(str) / 2; i < (int)strlen(str); i++)
+{
+printf("%c", str[i]);
+}
+}
