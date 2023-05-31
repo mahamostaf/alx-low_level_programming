df8f730d1da9a2d84ed91e9d3bf09736c420f690
mahamostaf <mahamoftah19@gmail.com>
2023-05-30 18:06:57 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _strcpy- print every other character
+* @dest: pointer a
+* @src: integer n
+* Return: char pointer
+*/
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
