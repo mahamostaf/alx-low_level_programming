b7d978ef40feed45409eb4bb86133ec63cc979bd
mahamostaf <mahamoftah19@gmail.com>
2023-05-30 17:04:37 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* puts2 - print every other character
+* @str: pointer s
+* Return: void
+*/
+void puts2(char *str)
+{
+int i;
+char tmp;
+for (i = 0; i < strlen(str); i++)
+{
+tmp = str[i];
+if ((int)tmp % 2 == 0)
+{
+printf("%c", str[i]);
+}
+str++;
+}
+}
