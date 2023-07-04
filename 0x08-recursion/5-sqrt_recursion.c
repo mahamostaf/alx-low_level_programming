c92c9331e627b375096f025cc81981f1c7924c0a
mahamostaf <mahamoftah19@gmail.com>
2023-07-03 20:40:21 -0700

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+
+/**
+* _sqrt_recursion - recursion
+* @n: int n
+* Return: int
+*/
+int _sqrt_recursion(int n)
+{
+if (sqrt(n) * sqrt(n) != n)
+return (-1);
+return sqrt(n);
+}
